45bd0b94a8ff3098ed60f0eb02553b6a43321ab2
Vinay Tanwar <tanwarv96@gmail.com>
2024-10-14 12:54:34 +0530

    excercise 1 - breakpoints added

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,16 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+// in px
+const BREAKPOINTS = {
+  phoneMax: 600,
+  tabletMax: 950,
+  laptopMax: 1300,
+}
+
+export const QUERIES = {
+  phoneAndDown: `(max-width: ${BREAKPOINTS.phoneMax / 16}rem)`,
+  tableAndDown: `(max-width: ${BREAKPOINTS.tabletMax / 16}rem)`,
+  laptopAndDown: `(max-width: ${BREAKPOINTS.laptopMax / 16}rem)`,
+}
